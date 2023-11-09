d02260d7882ecda94a015727ec24c75b83dab7fa
Иван Хомюк <mr.homyuk@mail.ru>
2023-11-09 14:35:57 +0900

    Almost done.

---
+++ backend/cats/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from cats.models import Achievement, Cat
+
+admin.site.empty_value_display = 'Не задано'
+
+
+class CatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'birth_year', 'owner')
+    search_fields = ('name', 'birth_year', 'owner')
+    list_display_links = ('name',)
+    list_filter = ('color', 'birth_year', 'owner')
+
+
+admin.site.register(Achievement)
+admin.site.register(Cat, CatAdmin)
